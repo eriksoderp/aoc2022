94cece9031d55caa9f438d0fc83ba8001e935760
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2022-12-02 11:55:48 +0100

    Add files via upload

---
+++ puzzle02.py
@@ -0,0 +1,38 @@
+points = {'X':1, 'Y':2, 'Z':3}
+s = "AXBYCZ"
+def main():
+    f = open("input02.txt", "r")
+    
+    total_a = 0
+    total_b = 0
+
+    for line in f.readlines():
+        total_a += get_points_a(line)
+        total_b += get_points_b(line)
+        
+    print("Part a:", total_a)
+    print("Part b:", total_b)
+
+def get_points_a(line, diff = 22):
+    opp, elf = line.split(' ')
+    elf = elf.strip()
+    res = (ord(elf) - ord(opp) + diff) % 3
+    if res == 0:
+        return (3 + points[elf])
+    elif res == 1:
+        return (6 + points[elf])
+    elif res == 2:
+        return points[elf]
+
+def get_points_b(line, diff = 22):
+    opp, elf = line.split(' ')
+    elf = elf.strip()
+    if elf == 'X':
+        return points[s[(s.find(opp) - 1) % 6]]
+    elif elf == 'Y':
+        return (3 + points[s[s.find(opp) + 1]])
+    elif elf == 'Z':
+        return (6 + points[s[(s.find(opp) + 3) % 6]])
+
+if __name__ == "__main__":
+    main()
