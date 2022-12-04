3e45ea0673bc70c64212153a975792b437d907fb
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2022-12-04 14:26:57 +0100

    Update puzzle01.py

--- puzzle01.py
+++ puzzle01.py
@@ -1,20 +1,19 @@
-def main():
-    f = open("input01.txt", "r")
-    
+def main():    
     i = 0
     sums = []
     currSum = 0
-    for line in f.readlines():
-        if line == "\n":
-            sums.append(currSum)
-            i += 1
-            currSum = 0
-        else:
-            currSum += int(line[:-1])
+    with open("input01.txt") as f:
+        for line in f.readlines():
+            if line == "\n":
+                sums.append(currSum)
+                i += 1
+                currSum = 0
+            else:
+                currSum += int(line[:-1])
 
     sums.sort()
-    print(sums[-1])
-    print(sum(sums[-3:]))
+    print("Part a:", sums[-1])
+    print("Part b:", sum(sums[-3:]))
 
 if __name__ == "__main__":
     main()
