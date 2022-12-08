126efe72e80134cc47f5e58f2fd0dd20c038b030
eriksoderp <54850702+eriksoderp@users.noreply.github.com>
2022-12-08 23:14:19 +0100

    Update puzzle06.py

--- puzzle06.py
+++ puzzle06.py
@@ -5,15 +5,13 @@ def main():
         b = False
         
         for i, _ in enumerate(l):
-            if not a:
-                if len(set(l[i:i-4:-1])) == 4:
-                    print("Part a:", i+1)
-                    a = True
+            if not a and len(set(l[i:i-4:-1])) == 4:
+                print(i+1)
+                a = True
 
-            if not b:
-                if len(set(l[i:i-14:-1])) == 14:
-                    print("Part b:", i+1)
-                    b = True
+            if not b and len(set(l[i:i-14:-1])) == 14:
+                print(i+1)
+                b = True
             
             if a and b: break
 
